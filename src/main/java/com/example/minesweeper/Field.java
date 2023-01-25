2b8fbea50931d84cd88f98caa3c3ae254effd263
PhyToN-xD <luki.bauer2005@gmail.com>
2023-01-25 08:09:47 +0100

    .

---
+++ src/main/java/com/example/minesweeper/Field.java
@@ -0,0 +1,125 @@
+package com.example.minesweeper;
+
+import javafx.scene.Node;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field extends StackPane {
+
+  /*
+  Variablen
+   */
+  private int x, y;
+  public boolean bomb;
+  private int count = 0;
+  private String text;
+  public Text bombCount;
+  private Rectangle fieldNode;
+  private boolean openedField;
+
+  /*
+  ArrayLists fuer Speichern vom Feld
+   */
+  private ArrayList<Integer> arrXBomb = new ArrayList<>();
+  private ArrayList<Integer> arrYBomb = new ArrayList<>();
+
+
+  public ArrayList<Integer> getArrXBomb() {
+    return arrXBomb;
+  }
+
+  public ArrayList<Integer> getArrYBomb() {
+    return arrYBomb;
+  }
+
+  /*
+  Getter, Setter, Konstruktor
+   */
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setBombCount(String bombCount) {
+    this.bombCount.setText(bombCount);
+  }
+
+  public Field(int x, int y, boolean bomb) {
+    this.x = x;
+    this.y = y;
+    this.bomb = bomb;
+    this.openedField = false;
+    openedField = false;
+    if (bomb) {
+      text = "X";
+      arrXBomb.add(x);
+      arrYBomb.add(y);
+      //System.out.println(arrXBomb + " | " + arrYBomb);
+    }
+
+    /*
+    Feld Gestaltung
+     */
+
+    fieldNode = new Rectangle(MinesweeperApplication.FIELD_SIZE - 2, MinesweeperApplication.FIELD_SIZE - 2);
+    fieldNode.setFill(Color.LIGHTBLUE);
+    fieldNode.setStroke(Color.LIGHTCYAN);
+    fieldNode.setVisible(true);
+
+    bombCount = new Text();
+    bombCount.setVisible(false);
+    bombCount.setText(this.bomb ? "X" : "");
+    bombCount.setStroke(Color.RED);
+    setOnMouseClicked(e -> onFieldClick(e));
+
+    getChildren().addAll(fieldNode, bombCount);
+    setTranslateX(x * MinesweeperApplication.FIELD_SIZE);
+    setTranslateY(y * MinesweeperApplication.FIELD_SIZE);
+    setOnMouseClicked(e -> onFieldClick(e));
+
+  }
+  /*
+  Auf Mausklick reagieren, Rechtsklick, Linksklick
+   */
+
+  private void onFieldClick(MouseEvent e) {
+    if (e.getButton() == MouseButton.PRIMARY) {
+      open();
+    } else if (e.getButton() == MouseButton.SECONDARY) {
+      fieldNode.setFill(Color.RED);
+      bombCount.setVisible(false);
+    }
+  }
+
+  /*
+  alle anliegenden Freien Felder werden geoeffnet
+   */
+
+  public void open(){
+    if (this.openedField) return;
+    this.openedField = true;
+    bombCount.setVisible(true);
+    fieldNode.setFill(Color.LIGHTGRAY);
+    if (bombCount.getText().isEmpty()) {
+      MinesweeperApplication.getNeighbours(this).forEach(Field::open);
+    }
+
+    /*if (!this.openedField && !this.bomb) {
+      fieldNode.setFill(Color.LIGHTGRAY);
+      bombCount.setVisible(true);
+      if (bombCount.getText().isEmpty()) {
+        MinesweeperApplication.getNeighbours(this).forEach(Field::open);
+      }
+    }*/
+  }
+}
--- src/main/java/com/example/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/example/minesweeper/MinesweeperApplication.java
@@ -2,22 +2,105 @@ package com.example.minesweeper;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinesweeperApplication extends Application {
+  private static final int X_FIELD = 16;
+  private static final int Y_FIELD = 16;
+  public static final int FIELD_SIZE = 20;
+  private static Field[][] grid = new Field[X_FIELD][Y_FIELD];
+  public static int countBomb = 0;
+
+
+
+
+  private Parent createContent () {
+    Pane root = new Pane();
+
+    root.setPrefSize(320, 326);
+
+    /*
+     Spielfeld mit Zufallsbomben wird generiert
+     */
+    for (int y = 0; y < Y_FIELD; y++) {
+      for (int x = 0; x < X_FIELD; x++) {
+        Field field = new Field(x, y, Math.random() < 0.13);
+        grid[x][y] = field;
+        root.getChildren().add(field);
+      }
+    }
+
+    for (int y = 0; y < Y_FIELD; y++) {
+      for (int x = 0; x < X_FIELD; x++) {
+        Field field = grid[x][y];
+        if (!field.bomb){
+          int count = 0;
+          ArrayList<Field> fields = getNeighbours(grid[x][y]);
+
+          for (Field f:fields) {
+            if (f.bomb) count++;
+          }
+          if (count > 0) {
+            field.setBombCount(Integer.toString(count));
+          }
+        }
+      }
+    }
+
+    return root;
+  }
+
+  /*
+  Herausfinden von den anliegenden Feldern
+   */
+
+  static ArrayList<Field> getNeighbours (Field field) {
+    ArrayList<Field> neighbours = new ArrayList<>();
+
+    int[] points = new int[] {-1,-1, -1,0, -1,1, 0,1, 1,1, 1,0, 1,-1, 0,-1};
+
+    for (int i = 0; i<points.length; i++) {
+      int dx = points[i];
+      int dy = points[i+1];
+
+      int newX = field.getX() + dx;
+      int newY = field.getY() + dy;
+
+      if (newX >= 0 && newX < X_FIELD && newY >= 0 && newY < Y_FIELD) {
+        neighbours.add(grid[newX][newY]);
+      }
+      i++;
+    }
+
+    return neighbours;
+  }
+
+
+
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(MinesweeperApplication.class.getResource("minesweeper-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
+    //FXMLLoader fxmlLoader = new FXMLLoader(MineSweeperApplication.class.getResource("hello-view.fxml"));
+
+    Scene scene = new Scene(createContent());
+
+    stage.setTitle("MineSweeper");
     stage.setScene(scene);
     stage.show();
+
+
   }
 
   public static void main(String[] args) {
     launch();
   }
+
+
 }
